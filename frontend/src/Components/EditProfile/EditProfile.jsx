4b148dc25a0cfc6c0b9e0c18503fd1fc6b63a496
DEBARGHYA MONDAL <mondaldebarghya14@gmail.com>
2023-04-04 01:12:33 +0530

    update

--- frontend/src/Components/EditProfile/EditProfile.jsx
+++ frontend/src/Components/EditProfile/EditProfile.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import {React,useState} from 'react'
 import './EditProfile.css'
 
 // Email Icon
@@ -7,21 +7,49 @@ import EmailIcon from '@mui/icons-material/Email';
 import ContactsIcon from '@mui/icons-material/Contacts';
 // Profile Icon
 import PersonIcon from '@mui/icons-material/Person';
+import axios from 'axios';
+
 
 const EditProfile = () => {
+    const [userdata,setuserdata]=useState({
+        name:'',
+        email:'',
+        phonenumber:''
+    })
+    const handelchange=(e)=>{
+        const name=e.target.name;
+        const value=e.target.value;
+        setuserdata({...userdata,[name]:value})
+    }
+    const handelsubmit=async(e)=>{
+        const token=localStorage.getItem("token");
+        const userid=localStorage.getItem("userid");
+        e.preventDefault();
+        axios.put(`http://localhost:5000/api/auth/${userid}`,userdata,{
+            headers:{
+                'Authorization':`Bearer ${token}`
+            }
+        }).then((res)=>{
+            window.location.reload();
+            console.log(res);
+        }
+        ).catch((err)=>{
+            console.log(err);
+        })
+    }
     return (
         <>
             <div className="edProf">
                 <label htmlFor="name"><PersonIcon style={{ marginRight: '5px' }} />Name</label>
-                <input type="text" id='name' name='name' />
+                <input type="text" id='name' name='name' value={userdata.name} onChange={handelchange} />
 
                 <label htmlFor="email"><EmailIcon style={{ marginRight: '5px' }} />Email</label>
-                <input type="email" name='email' id='email' />
+                <input type="email" name='email' id='email' value={userdata.email} onChange={handelchange}/>
 
                 <label htmlFor="name"><ContactsIcon style={{ marginRight: '5px' }} />Contact</label>
-                <input type="text" name='contact' id='contact' />
+                <input type="text" name='phonenumber' id='contact' value={userdata.phonenumber} onChange={handelchange}/>
 
-                <button className="btn btn-outline-success">Save</button>
+                <button className="btn btn-outline-success" onClick={handelsubmit}>Save</button>
             </div>
         </>
     )
