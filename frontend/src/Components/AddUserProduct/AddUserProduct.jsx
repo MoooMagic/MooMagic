2de3894ff49daace573e182dd5e8ea80d65d6122
shilajit2002 <shilajit.acharjee@gmail.com>
2023-04-06 01:10:10 +0530

    Update

--- frontend/src/Components/AddUserProduct/AddUserProduct.jsx
+++ frontend/src/Components/AddUserProduct/AddUserProduct.jsx
@@ -1,12 +1,16 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "./AddUserProduct.css";
 import axios from "axios";
+
+import { useNavigate } from "react-router-dom";
 
 // Sweetalert
 import Swal from "sweetalert2";
 
 const AddUserProduct = () => {
-  const[addProduct, setAddProduct] = useState({
+  const navigate = useNavigate();
+
+  const [addProduct, setAddProduct] = useState({
     product_img: "",
     product_name: "",
     product_desc: "",
@@ -23,37 +27,48 @@ const AddUserProduct = () => {
       [name]: value,
     });
   };
-  const token=localStorage.getItem('token');
-  console.log(token);
+  const token = localStorage.getItem('token');
+  // console.log(token);
   const uploadProduct = () => {
-    axios.post('http://localhost:5000/api/product/addproduct', addProduct,{
-      headers: {
-        'Authorization': `Bearer ${token}`,}
-    }).then((res) => {
-      console.log(res);
-      Swal.fire({
-        position: 'top-end',
-        icon: 'success',
-        title: 'Product Added Successfully',
-        showConfirmButton: false,
-        timer: 1500
+    if (addProduct.product_img !== "" && addProduct.product_name !== "" && addProduct.product_desc !== "" && addProduct.price !== 0 && addProduct.quantity !== "") {
+      axios.post('http://localhost:5000/api/product/addproduct', addProduct, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        }
+      }).then((res) => {
+        console.log(res);
+        Swal.fire({
+          icon: "success",
+          title: 'Product added successfully !!',
+          confirmButtonText: 'Ok',
+        }).then((result) => {
+          if (result.isConfirmed) {
+            navigate('/user-product');
+          } else {
+            navigate('/user-product');
+          }
+        })
+      }).catch((err) => {
+        console.log(err);
+      });
+      setAddProduct({
+        product_img: "",
+        product_name: "",
+        product_desc: "",
+        price: 0,
+        quantity: "",
+        InStock: true,
       })
-    }).catch((err) => {
-      console.log(err);
-    });
-    setAddProduct({product_img: "",
-    product_name: "",
-    product_desc: "",
-    price: 0,
-    quantity: "",
-    InStock: true,})
+    } else {
+      alert("Invalid Input");
+    }
   };
 
   return (
     <>
       <div className="AddUserProduct">
         <div className="container">
-          <h3 className="text-center">Add Your Own Product</h3>
+          <h3 className="text-center my-3">Add Your Own Product</h3>
           <label htmlFor="product_img">Enter Product Img URL</label>
           <input
             type="text"
--- frontend/src/Components/UserProduct/UserProduct.jsx
+++ frontend/src/Components/UserProduct/UserProduct.jsx
@@ -3,87 +3,112 @@ import "./UserProduct.css";
 
 import { useNavigate } from "react-router-dom";
 
+import { Link } from "react-router-dom";
+
 // Rupee
 import CurrencyRupeeIcon from "@mui/icons-material/CurrencyRupee";
+// Edit Icon
+import EditIcon from '@mui/icons-material/Edit';
+// Delete Icon
+import DeleteIcon from '@mui/icons-material/Delete';
+
 import axios from "axios";
-
-
-
 
 const UserProduct = () => {
   const navigate = useNavigate();
-  
+
   const [showUserProd, setshowUserProd] = useState([]);
 
   useEffect(() => {
-    const token=localStorage.getItem("token");
-    const userid=localStorage.getItem("userid");
-    if(!token&&!userid){
-      return window.location.href="/signin";
+    const token = localStorage.getItem("token");
+    const userid = localStorage.getItem("userid");
+    if (!token && !userid) {
+      return window.location.href = "/signin";
     }
-    axios.get(`http://localhost:5000/api/product/productbyuser/${userid}`,{
-      headers:{
-        'Authorization':`Bearer ${token}`
-      }
-    }).then((res)=>{
-      setshowUserProd(res.data);
-    }).catch((err)=>{
-      console.log(err);
-    })
+    else {
+      axios.get(`http://localhost:5000/api/product/productbyuser/${userid}`, {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      }).then((res) => {
+        setshowUserProd(res.data);
+      }).catch((err) => {
+        console.log(err);
+      })
+    }
+
   }, [])
+
+  const deletePerProduct = () => {
+
+  }
+
+  const editPerProduct = () => {
+
+  }
+
   return (
     <>
       <div className="userProduct">
-        {showUserProd.length !== 0 ? (
-          showUserProd.map((elem, index) => (
-            <div className="card" style={{ width: "17rem" }} key={index}>
-              {/* Product Img */}
-              <img src={elem.product_img} alt="" className="card-img-top" />
-              {/* Product Body */}
-              <div className="card-body">
-                {/* Product Name */}
-                <h5 className="card-title">{elem.product_name}</h5>
-                {/* Product Tetx */}
-                <p className="card-text">{elem.product_desc}</p>
-                {/* In Stock */}
-                <button
-                  className="stock"
-                  style={{
-                    backgroundColor: elem.InStock
-                      ? "dodgerblue"
-                      : "rgb(255, 70, 45)",
-                  }}
-                >
-                  {elem.InStock ? "In Stock" : "Out of Stock"}
-                </button>
-                {/* Price */}
-                <div className="price">
-                  <p>
-                    <strong>
-                      <CurrencyRupeeIcon />
-                      {elem.price}
-                    </strong>
-                  </p>
-                  <p className="fadePrice">{elem.quantity}</p>
+        <button
+          className="btn btn-outline-primary"
+          onClick={() => {
+            navigate("/add-product")
+          }}
+        >
+          Add Product
+        </button>
+
+        <div className="userProductSide">
+          {showUserProd.length !== 0 ? (
+            showUserProd.map((elem, index) => (
+              <div className="card" style={{ width: "17rem" }} key={index}>
+                {/* Product Img */}
+                <img src={elem.product_img} alt="" className="card-img-top" />
+                {/* Product Body */}
+                <div className="card-body">
+                  {/* Product Name */}
+                  <h5 className="card-title">{elem.product_name}</h5>
+                  {/* Product Tetx */}
+                  <p className="card-text">{elem.product_desc}</p>
+                  {/* In Stock */}
+                  <button
+                    className="stock"
+                    style={{
+                      backgroundColor: elem.InStock
+                        ? "dodgerblue"
+                        : "rgb(255, 70, 45)",
+                    }}
+                  >
+                    {elem.InStock ? "In Stock" : "Out of Stock"}
+                  </button>
+                  {/* Price */}
+                  <div className="price">
+                    <p>
+                      <strong>
+                        <CurrencyRupeeIcon />
+                        {elem.price}
+                      </strong>
+                    </p>
+                    <p className="fadePrice">{elem.quantity}</p>
+                  </div>
+                  <div className="lowerBtn">
+                    <EditIcon style={{ color: 'blue' }} onClick={editPerProduct} />
+
+                    <Link to={`/products/${elem.id}`} className="btn btn-outline-info my-2">View</Link>
+
+                    <DeleteIcon style={{ color: 'red' }} onClick={deletePerProduct} />
+                  </div>
                 </div>
               </div>
+            ))
+          ) : (
+            <div className="text-center container">
+              <h4>No Product have been added (`~`)</h4>
             </div>
-          ))
-        ) : (
-          <div className="text-center container">
-            <h4>No Results Found (`~`)</h4>
-          </div>
-        )}
+          )}
+        </div>
       </div>
-
-      <button
-        className="btn btn-outline-primary"
-        onClick={() => {
-          navigate("/add-product")
-        }}
-      >
-        Add Product
-      </button>
     </>
   );
 };
