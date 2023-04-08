2e7e322a145a973f1a430e54b086722901129abb
Anirban Samanta <ani.samanta365@outlook.com>
2023-04-08 13:24:55 +0530

    responsiveness updated

--- frontend/src/Components/SignUp/SignUp.jsx
+++ frontend/src/Components/SignUp/SignUp.jsx
@@ -13,13 +13,13 @@ const SignUp = () => {
       <div className="signUpMain">
         <div className="container">
           <div class="card" style={{ width: "18rem",height:"350px" }}>
-            <img class="card-img-top" src={seller} alt="Card image" />
+            <img class="card-img-top" src={seller} alt="Card-img" />
             <div class="card-body">
               <h5 class="card-title text-center" style={{cursor:'pointer'}} onClick={()=>{navigate('/signup-seller')}}>Sign Up as Seller</h5>
             </div>
           </div>
           <div class="card" style={{ width: "18rem",height:"350px" }}>
-            <img class="card-img-top" src={retailer} alt="Card image" />
+            <img class="card-img-top" src={retailer} alt="Card-img" />
             <div class="card-body">
               <h5 class="card-title text-center" style={{cursor:'pointer'}} onClick={()=>{navigate('/signup-retailer')}}>Sign Up as Retailer</h5>
             </div>
